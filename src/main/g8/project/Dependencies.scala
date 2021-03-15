e9046710d55f78ff0fb161aac762a9dd14c84d6f
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-03-15 14:16:37 +0000

    PLATUI-1060: Uses Gatling 3.4.2 and performance-test-runner 4.5.0
    New gatling.conf to match 3.4.2 version.
    Updated logback.xml to match 3.4.2 version and surface performance-test-runner logs.

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "2.3.1"
+  private val gatlingVersion = "3.4.2"
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.3.1"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "3.10.0"        % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "4.5.0"        % Test,
     "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test
   )
