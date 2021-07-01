dbb55bfca56da37b60dff9b5ad16d3922219128f
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-07-01 16:22:46 +0100

    PLATUI-1224: Update performance-test-runner to use 4.11.0
    This removes `maxDuration` config in the Simulation Setup.

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,9 +5,9 @@ object Dependencies {
   private val gatlingVersion = "3.4.2"
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.3.1"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "4.9.0"        % Test,
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test
+    "com.typesafe"          % "config"                    % "1.3.1"         % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "4.11.0"        % Test,
+    "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
+    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
   )
 }
