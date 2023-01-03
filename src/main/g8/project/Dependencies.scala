4cfd6a400ba2a9b3aa674decf6709ffc8fa30955
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2023-01-03 11:40:19 +0000

    PLATUI-2177 - fixing formatting

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.3.1"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.4.0"        % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.4.0"         % Test,
     "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
   )
