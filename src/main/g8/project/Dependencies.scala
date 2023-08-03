5e93a31677334c73699e2c6215ec217715550bc7
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2023-08-03 14:28:05 +0100

    PLATUI-BAU: Update typesafe config to version 1.4.2

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,9 +5,10 @@ object Dependencies {
   private val gatlingVersion = "3.6.1"
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.3.1"         % Test,
+    "com.typesafe"          % "config"                    % "1.4.2"         % Test,
     "uk.gov.hmrc"          %% "performance-test-runner"   % "5.6.0"         % Test,
     "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
   )
+
 }
